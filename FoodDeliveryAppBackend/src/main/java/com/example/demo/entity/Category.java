6e6058d2f638c252050538ddb5798603f3f9242d
PoojaPanhale <poojapanhale6@gmail.com>
2023-05-09 21:49:26 +0530

    commit message

---
+++ FoodDeliveryAppBackend/src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Getter
+@Setter
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_product_category")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> product;
+}
